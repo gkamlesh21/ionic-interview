c497d647cd5fce76d098ab1cc8d25af42b48d184
Kamlesh Gupta <gkamlesh21@gmail.com>
2023-03-24 19:51:08 +0530

    revamped angular branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+// import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+// import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FilterPipeModule } from "ngx-filter-pipe";
+// import { AppMinimize } from '@ionic-native/app-minimize/ngx';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    FilterPipeModule
+  ],
+  providers: [
+    StatusBar,
+    // SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // AppMinimize
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  
+}
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DataService } from '../service/data.service';
+import { DataModel } from '../service/dataModel';
+import { Platform } from '@ionic/angular';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  selector: "app-folder",
+  templateUrl: "./folder.page.html",
+  styleUrls: ["./folder.page.scss"],
 })
 export class FolderPage implements OnInit {
-  public folder!: string;
+  public folder: any;
+  public technologies: Array<{
+    id: string;
+    name: string;
+    description: string;
+  }> | undefined;
+  searchedKeyword: any = { name: '' };
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private dataService: DataService, private platform: Platform, private location: Location) {}
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-  }
+    // console.log(this.location);
+    this.folder = this.activatedRoute.snapshot.paramMap.get("id");   
+    
+    this.dataService.getFolderDataPromise()
+        
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (this.router.getCurrentNavigation()?.extras?.state) {
+        // console.log(this.router.getCurrentNavigation().extras.state.singleData);
+        this.technologies = this.router.getCurrentNavigation()?.extras?.state?.["singleData"]?.question;
+      } else {
+         this.dataService.getFolderData()
+          .subscribe(
+            (data: DataModel[] ) => {
+              // console.log(data, this.folder, this.folder.toLowerCase() );
 
+              let _this = this;
+              const temp = data?.find(item => this.folder.toLowerCase() == item.title.toLowerCase());
+              
+              // console.log(temp);
+              this.technologies = (temp).question;
+            },
+            (err: any) => {
+              console.log(err);
+            }
+          );
+      }
+    });
+
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.searchedKeyword.name = "";
+      // this.location.back();
+    });
+  }
 }
